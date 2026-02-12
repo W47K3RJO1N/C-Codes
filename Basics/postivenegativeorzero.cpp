07d42ab975a3f00633b9c9d0e8f14d1f3c1ef172
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-02-12 22:59:27 +0530

    Add files via upload

---
+++ Basics/postivenegativeorzero.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+    if ( a > 0 ) {
+        cout << "The Entered number is Postive";
+    }
+
+    if ( a < 0) {
+        cout << "The Entered number is negative";
+    }
+
+   if (a = 0) {
+        cout << "The Entered number is Zero";
+    }
+    return 0;
+}
