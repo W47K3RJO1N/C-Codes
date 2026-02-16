a033d34afb8849e757bed6cb6d9e133a00831e8c
Himanshu Bhirud <116067299+W47K3RJO1N@users.noreply.github.com>
2026-02-16 23:19:08 +0530

    Add files via upload

---
+++ SET/count.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    set<int> s;
+
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(6);
+    s.insert(7);
+
+    // s.insert(6); // stores only unique element
+
+    cout << "lower bound = " << *(s.lower_bound(4)) << endl;
+
+    cout << s.size() << endl;
+    for (auto val : s) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
